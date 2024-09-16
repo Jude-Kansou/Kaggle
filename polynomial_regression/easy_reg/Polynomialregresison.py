e68a542ba20a53cbd74672efa02dfcf2e5f38114
Jude Kansou <learningarboreal@proton.me>
2024-09-16 10:57:38 -0400

    easy reg done

--- polynomial_regression/easy_reg/Polynomialregresison.py
+++ polynomial_regression/easy_reg/Polynomialregresison.py
@@ -1,25 +1,91 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 import seaborn as sns
+import os
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from joblib import dump, load
 
+
 file_path = os.path.join(os.getcwd(), "Realestate.csv")
 df = pd.read_csv(file_path)
 
+# sns.pairplot(df)
+# plt.show()
+
+X = df.drop("Y house price of unit area", axis=1)
+y = df['Y house price of unit area']
+
+"""
+polynomial_converter = PolynomialFeatures(degree=2, include_bias=False)
+poly_features = polynomial_converter.fit(X)
+poly_features = polynomial_converter.transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    poly_features, y, test_size=0.3, random_state=101)
+
+polymodel=LinearRegression()
+polymodel.fit(X_train, y_train)
+
+# print(polymodel.coef_)
+
+y_pred=polymodel.predict(X_test)
+
+MAE_Poly = metrics.mean_absolute_error(y_test,y_pred)
+MSE_Poly = metrics.mean_squared_error(y_test,y_pred)
+RMSE_Poly = np.sqrt(MSE_Poly)
+
+checker = pd.DataFrame([MAE_Poly, MSE_Poly, RMSE_Poly],
+             index=['MAE', 'MSE', 'RMSE'], columns=['metrics'])
+"""
+
+train_RMSE_list=[]
+#Test List of RMSE per degree
+test_RMSE_list=[]
+
+for d in range(1,10):
+    
+    #Preprocessing
+    #create poly data set for degree (d)
+    polynomial_converter= PolynomialFeatures(degree=d, include_bias=False)
+    poly_features= polynomial_converter.fit(X)
+    poly_features= polynomial_converter.transform(X)
+    
+    #Split the dataset
+    X_train, X_test, y_train, y_test = train_test_split(poly_features, y, test_size=0.3, random_state=101)
+    
+    #Train the Model
+    polymodel=LinearRegression()
+    polymodel.fit(X_train, y_train)
+    
+    #Predicting on both Train & Test Data
+    y_train_pred=polymodel.predict(X_train)
+    y_test_pred=polymodel.predict(X_test)
+    
+    #Evaluating the Model
+    
+    #RMSE of Train set
+    train_RMSE=np.sqrt(metrics.mean_squared_error(y_train, y_train_pred))
+    
+    #RMSE of Test Set
+    test_RMSE=np.sqrt(metrics.mean_squared_error(y_test, y_test_pred))
+    
+    #Append the RMSE to the Train and Test List
+    
+    train_RMSE_list.append(train_RMSE)
+    test_RMSE_list.append(test_RMSE)
 
 
-print(df)
+train_RMSE_list = pd.DataFrame({'Train RMSE List':train_RMSE_list})
 
+test_RMSE_list = pd.DataFrame({'Ttest RMSE List':test_RMSE_list})
 
-
-
-
-
-
-
+plt.plot(range(1,6), train_RMSE_list[:5], label='Train RMSE')
+plt.plot(range(1,6), test_RMSE_list[:5], label='Test RMSE')
+plt.xlabel('Polynomial Degree')
+plt.ylabel('RMSE')
+plt.legend()
+plt.show()
